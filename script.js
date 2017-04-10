40ae3df35ce22c6b286ed74841ee9e3c4581721b
MarekM <mrkm090@gmail.com>
2017-04-10 21:13:45 +0200

    1.2 FizzBuzz

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-for(var i = "#"; i.length < 8; i += "#") {
-  console.log(i);
+/*for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);    
+  }
+}
+*/
+// Second version
+
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
 }
