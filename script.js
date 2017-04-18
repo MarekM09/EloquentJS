9f8da0d0f3e460c70c0a4282f7636808234d766c
MarekM <mrkm090@gmail.com>
2017-04-18 16:44:36 +0200

    2.3 Bean counting

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function isEven(number) {
-  if(number < 0)
-  	return isEven(-number);
-  else if (number == 0)
-  	return true;
-  else if (number == 1)
-  	return false;
-  else
-  	return isEven(number - 2);
+function countChar(str, chr) {
+	var counter = 0;
+
+	for(var i = 0; i < str.length; i++)
+	{
+		if (str.charAt(i) === chr) {
+			counter++
+		}
+	}
+	return counter;
 }
 
-console.log(isEven(-50));
+function countBs(str) {
+	return countChar(str, "B")
+}
+
+console.log(countChar("kakkerlak", "k"));
+console.log(countBs("BBC"));
